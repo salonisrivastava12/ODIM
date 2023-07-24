ab4059e3306f4a904d7b5b2c7441ee63a29cfe35
Srivastava <saloni.srivastava@hpe.com>
2023-07-24 14:17:38 +0530

    BRUCE-7667

--- svc-account-session/session/create.go
+++ svc-account-session/session/create.go
@@ -144,14 +144,13 @@ func CreateNewSession(ctx context.Context, req *sessionproto.SessionCreateReques
 	resp.Header = map[string]string{
 		"Link":         "</redfish/v1/SessionService/Sessions/" + sess.ID + "/>; rel=self",
 		"X-Auth-Token": sess.Token,
-		"Location":"</redfish/v1/SessionService/Sessions/" + sess.ID + "/>",
-		
+		"Location":      "</redfish/v1/SessionService/Sessions/" + sess.ID + "/>; rel=self",
+
 	}
 	l.LogWithFields(ctx).Info("resp.Header>>>>>>>>",resp.Header)
 	commonResponse.ID = sess.ID
 	commonResponse.OdataID = "/redfish/v1/SessionService/Sessions/" + commonResponse.ID
 	commonResponse.CreateGenericResponse(resp.StatusMessage)
-	l.LogWithFields(ctx).Info("OdataID >>>>",commonResponse.OdataID)
 	resp.Body = asresponse.Session{
 		Response: commonResponse,
 		UserName: createSession.UserName,
